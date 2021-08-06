a55d7c18b06279577a6735ab864e14d546f048e6
Deyan <43583063+deyansp@users.noreply.github.com>
2021-08-06 13:54:07 +0300

    initial HTML for NoteEditor component

--- src/NoteEditor.js
+++ src/NoteEditor.js
@@ -3,7 +3,13 @@ import React from "react";
 export default function NoteEditor(props) {
   return (
     <div className="note-editor">
-      <p>NoteEditor</p>
+      <div className="note-editor-header">
+        <h2>Edit Note</h2>
+      </div>
+      <div className="note-editor-text">
+        <textarea placehodler="Start typing here..."></textarea>
+      </div>
+      <button>Save</button>
     </div>
   );
 }
