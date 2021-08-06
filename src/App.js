835b49f92cb3545f2fde7f66b3f25bcb65ae302e
Deyan <43583063+deyansp@users.noreply.github.com>
2021-08-06 14:58:25 +0300

    added method for saving note text changes

--- src/App.js
+++ src/App.js
@@ -19,7 +19,19 @@ export default function App() {
     setNotes([newNote, ...notes]);
   };
 
-  const saveNote = () => {};
+  const saveNote = (id, text) => {
+    // making a copy of the notes array in state
+    let tempNotes = [...notes];
+
+    // finding the id of the note that needs to be changed
+    let noteIndex = tempNotes.findIndex((note) => note.id === id);
+
+    // updating its value in the copy of the state
+    tempNotes[noteIndex].text = text;
+
+    // updating state
+    setNotes([tempNotes]);
+  };
 
   const setActiveNote = () => {};
 
