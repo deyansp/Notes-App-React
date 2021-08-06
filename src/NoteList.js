de1a38b4ef4d9fe9df847d9db104fe6d1c787f93
Deyan <43583063+deyansp@users.noreply.github.com>
2021-08-06 13:48:51 +0300

    initial HTML layout for NoteList component

--- src/NoteList.js
+++ src/NoteList.js
@@ -3,7 +3,20 @@ import React, { useState } from "react";
 export default function NoteList(props) {
   return (
     <div className="note-list">
-      <p>NoteList</p>
+      <div className="note-list-header">
+        <h2>My Notes</h2>
+        <button>New Note</button>
+      </div>
+      <div className="note-list-body">
+        <div className="note-list-item">
+          <div className="note-list-item-title">
+            <h4>FIRST TEXT LINE</h4>
+          </div>
+          <div className="note-list-item-date">
+            <p>Created: </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
